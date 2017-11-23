05f28efe6cba3f9da647e18102ef0f0a841f445f
unknown <680755@W68-680755.internet.com>
2017-11-23 09:35:46 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/a680755/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.a680755.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 680755 on 2017/11/23.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
